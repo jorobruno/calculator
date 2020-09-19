15e56eddfd9d22d46cdf3df3316a64fe23b0a0ea
jorobruno <jorobruno@gmail.com>
2020-09-19 19:13:00 -0300

    Proyecto terminado -ahora si

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const delButton = document.querySelector(`[data-del]`);
 const acButton = document.querySelector(`[data-ac]`);
 const line1Element = document.querySelector(`[data-line-1]`);
 const line2Element = document.querySelector(`[data-line-2]`);
-let operation = ``;
+let operation;
+let n1 = 0;
+let n2 = 0;
 
 //FUNCIONES
 function addNumber(n) {
@@ -16,17 +18,29 @@ function addNumber(n) {
 
 
 function makeOperation(op) {
+    //Operaciones solas no funcionan
     if (line2Element.innerText == ``) return;
-    line1Element.innerText = line2Element.innerText + op;
-    line2Element.innerText = ``;
-    operation = op;
-
+    //Realizar operaciones 
+    if (line1Element.innerText == ``) {
+        n1 = parseFloat(line2Element.innerText);
+        line1Element.innerText = n1.toString(10) + op;
+        line2Element.innerText = ``;
+        operation = op;
+    }
+    if (line1Element.innerText != `` && line2Element.innerText != ``) {
+        n1 = parseFloat(line1Element.innerText);
+        n2 = parseFloat(line2Element.innerText);
+        let ret = processOperation(operation);
+        line1Element.innerText = ret.toString(10) + op;
+        line2Element.innerText = ``;
+        operation = op;
+    }
 }
 
 function processOperation(op) {
-    let ret;
-    let n1 = parseFloat(line1Element.innerText);
-    let n2 = parseFloat(line2Element.innerText);
+    let ret = 0;
+    n1 = parseFloat(line1Element.innerText);
+    n2 = parseFloat(line2Element.innerText);
     if (isNaN(n1) || isNaN(n2)) return;
     switch (op) {
         case `+`:
@@ -44,9 +58,8 @@ function processOperation(op) {
         default:
             return;
     }
-    line1Element.innerText = ``;
-    line2Element.innerText = ret.toString(10);
     operation = ``;
+    return ret;
 }
 
 function clearScreen() {
@@ -59,10 +72,15 @@ function deleteNum() {
     line2Element.innerText = line2Element.innerText.slice(0, -1);
 }
 
+function displayResult(result) {
+    clearScreen();
+    line2Element.innerText = result.toString(10);
+}
+
 
 // ESTRUCTURA
 numButtons.forEach(button => button.addEventListener(`click`, () => addNumber(button.innerText)));
 opButtons.forEach(button => button.addEventListener(`click`, () => makeOperation(button.innerText)));
-eqButton.addEventListener(`click`, () => processOperation(operation));
+eqButton.addEventListener(`click`, () => displayResult(processOperation(operation)));
 acButton.addEventListener(`click`, () => clearScreen());
 delButton.addEventListener(`click`, () => deleteNum());
